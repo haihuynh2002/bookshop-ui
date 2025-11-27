ec30756f0e5428374e9ad9cedfa1587b497a99c4
haihuynh2002 <info.haihuynh@gmail.com>
2025-11-27 18:26:23 +0700

    feat: basic ui for bookshop

---
+++ src/main/java/com/bookshop/bookshop_ui/BookshopUiApplication.java
@@ -0,0 +1,13 @@
+package com.bookshop.bookshop_ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookshopUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookshopUiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookshop/bookshop_ui/BookshopUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookshop.bookshop_ui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookshopUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
